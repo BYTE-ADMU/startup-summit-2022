4fa58eb7cd8d2d5ae80bc0f7128bded45fb7d775
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2022-03-19 22:31:12 +0800

    Finalized the look of the planet. Made multiple planets. Adjusted css and Startups.js accordingly.

--- src/pages/Startups/Planet.js
+++ src/pages/Startups/Planet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './styles.css'
 
-export default function Startups(){
+export default function Planet(){
     return (
         <div className="startups__container">
             <div className="planet__border">
--- src/pages/Startups/Startups.js
+++ src/pages/Startups/Startups.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import './styles.css'
-import './Planet.js'
+import Planet from './Planet';
 
 export default function Startups(){
     return (
         <div className="startups__container">
-            
+            <div className='startups__content'>
+                <div className='startup__planet__row'>
+                    <Planet />
+                    <Planet />
+                    <Planet />
+                </div>
+                <div className='startup__planet__row'>
+                    <Planet />
+                    <Planet />
+                    <Planet />
+                </div>
+                <div className='startup__planet__row startup__pagename'>
+                    <h1>Startups</h1>
+                </div>
+                <div className='startup__planet__row'>
+                    <Planet />
+                    <Planet />
+                    <Planet />
+                </div>
+                <div className='startup__planet__row'>
+                    <Planet />
+                    <Planet />
+                    <Planet />
+                </div>
+            </div>
         </div>
         
     )
