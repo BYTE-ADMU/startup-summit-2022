7f4155124ecf879a3691fadee070414edafa503d
M Jim Lee <m.lee@obf.ateneo.edu>
2022-03-23 11:03:05 +0800

    changed landing, worked on testimonials

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,31 +1,30 @@
 import React from 'react'
 import './styles.css'
-// import Button from '../../components/Button';
+import Button from '../../components/Button';
 
-// Landing Page Assets
-import NowBoarding from '../../assets/Landing/landing__nowboarding.svg'
-import MainLogo from '../../assets/Landing/landing__mainlogo.svg'
-import LYP from '../../assets/Landing/landing__launchyourpurpose.svg'
-import SYJ from '../../assets/Landing/landing__startyourjourney.svg'
+// Landing Page Image Assets
+import NowBoarding from './assets/landing__nowboarding.svg'
+import MainLogo from './assets/landing__mainlogo.svg'
+import TopLeft from './assets/landing__top-left.svg'
+import TopRight from './assets/landing__top-right.svg'
 
 export default function Landing(){
     return (
-        <div class="landing_container">
-            <div class="landing_content">
+        <div class="landing__container">
+            <div class="landing__content">
                 <img id="landing__nowboarding" src={NowBoarding} />
                 <img id="landing__mainlogo" src={MainLogo} />
                 <div id="landing__header">
-                <p>Launch Your Purpose</p>
-                <p>April 6-9, 2022</p>
-                    <div class="landing__buttons">
-                        {/* <Button style="landing" label="Register" link="register" /> */}
-                        {/* <Button style="landing" label="View Schedule" link="register" /> */}
-                    </div>
+                    <p>Launch Your Purpose</p>
+                    <p>April 6 - 9, 2022</p>
+                        <div class="landing__buttons">
+                            <Button style="primary landing-button" label="Register" />
+                            <Button style="secondary landing-button" label="View Schedule" />
+                        </div>
                 </div>
-                <div class="landing__stickers" >
-                <img id="landing__launchyourpurpose" src={LYP} />
-                <img id="landing__startyourjourney" src={SYJ} />
-                </div>
+            </div>
+            <div className="landing__top-left">
+                <img id="landing__top-left" src={TopLeft} />
             </div>
         </div>
     )
