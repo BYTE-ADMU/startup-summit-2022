886d2b3fe06a90af8b5f9f63c81d0ffdc181a8a9
M Jim Lee <m.lee@obf.ateneo.edu>
2022-03-24 14:03:03 +0800

    added about sus background

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 import './styles.css'
 
 // About Page Image Assets
-import AboutSusImage from './assets/abt-sus-img.png'
+import AboutSusImage from './assets/about-sus-img.png'
+import Spirals from '../../assets/bg-spirals.png'
+import Plane from './assets/about-plane.png'
+
 
 export default function About(){
     return (
@@ -16,6 +19,12 @@ export default function About(){
                     </p>
                     <p>Join us as we discover more about the ideation, technicalities, and adversities of a startup by learning from a high-caliber roster of speakers!</p>
                 </div>
+            </div>
+            <div className="about__bg">
+                <img src={Spirals} id="about__spirals1" />
+                <img src={Spirals} id="about__spirals2" />
+                <img src={Plane} id="about__plane1" />
+                <img src={Plane} id="about__plane2" />
             </div>
         </div>
     )
