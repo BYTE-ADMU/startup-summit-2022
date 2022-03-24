e610afee43f514f9aa871595cee97939bc07f137
M Jim Lee <m.lee@obf.ateneo.edu>
2022-03-25 02:42:23 +0800

    initial footer commit

---
+++ src/pages/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+
+export default function Footer(){
+    return (
+        <div className='footer__container'>
+            <div className="top-content">
+
+            </div>
+            
+        </div>
+    )
+}
