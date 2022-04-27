9e856f1ec2b380e42ab2db21be29fa7d142cf7bc
M Jim Lee <72350323+mjimlee@users.noreply.github.com>
2022-04-27 21:31:33 +0800

    commented out internships portion

--- src/pages/Internships/Internships.js
+++ src/pages/Internships/Internships.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import './styles.css'
+// import React from 'react'
+// import './styles.css'
 
-export default function Internships(){
-    return (
-        <div className="internship__container">
-            <div className="internship__content">
-                <div id="internship__header">
-                    <p>Launch Your Purpose</p>
-                    <p>April 6 - 9, 2022</p>
-                        <div class="internship__buttons">
-                            <a href="https://tinyurl.com/SUS2022RegForm/" id="internship-register-btn" target="_blank" rel="noreferrer">Register</a>
-                            <a href="#schedule" id="internship-schedule-btn">Schedule</a>
-                        </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+// export default function Internships(){
+//     return (
+//         <div className="internship__container">
+//             <div className="internship__content">
+//                 <div id="internship__header">
+//                     <p>Launch Your Purpose</p>
+//                     <p>April 6 - 9, 2022</p>
+//                         <div class="internship__buttons">
+//                             <a href="https://tinyurl.com/SUS2022RegForm/" id="internship-register-btn" target="_blank" rel="noreferrer">Register</a>
+//                             <a href="#schedule" id="internship-schedule-btn">Schedule</a>
+//                         </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
